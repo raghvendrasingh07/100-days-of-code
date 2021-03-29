de0dda9df11e92207d3f69139a37ba5cae6b866a
Raghvendra Singh <raghvendrapratap.success@gmail.com>
2021-03-29 07:52:36 +0530

    update

---
+++ Redux/index.js
@@ -0,0 +1,72 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+const Buy_Book = "Buy_Book";
+
+const bookInitialState = {
+  numberOfBooks: 10,
+  numberOfBookSold: 0,
+};
+const penInitialState = {
+  numberOfPens: 20,
+  numberOfPensSold: 0,
+};
+
+function buyBook() {
+  return {
+    type: Buy_Book,
+    payload: "Book Records",
+  };
+}
+function buyPen() {
+  return {
+    type: "Buy_Pen",
+    payload: "Pen Records",
+  };
+}
+
+const BookReducer = (state = bookInitialState, action) => {
+  switch (action.type) {
+    case "Buy_Book":
+      return {
+        ...state,
+        numberOfBooks: state.numberOfBooks - 1,
+        numberOfBookSold: state.numberOfBookSold + 1,
+      };
+    default:
+      return state;
+  }
+};
+const PenReducer = (state = penInitialState, action) => {
+  switch (action.type) {
+    case "Buy_Pen":
+      return {
+        ...state,
+        numberOfPens: state.numberOfPens - 1,
+        numberOfPensSold: state.numberOfPensSold + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const Reducer = combineReducers({
+  Book: BookReducer,
+  Pen: PenReducer,
+});
+
+const store = createStore(Reducer);
+console.log("Initial State ", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State Value ", store.getState());
+});
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyPen());
+store.dispatch(buyPen());
+store.dispatch(buyPen());
+store.dispatch(buyPen());
+unsubscribe();
